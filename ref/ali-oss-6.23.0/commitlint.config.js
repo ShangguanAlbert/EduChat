4b311427a86be5a2a2cccc0bb1e661ca69b0d8b6
Burning woods <tabc20030607@outlook.com>
2026-07-22 15:17:02 +0800

    chore: remove tracked third-party reference source

--- ref/ali-oss-6.23.0/commitlint.config.js
+++
@@ -1,1 +0,0 @@
-module.exports = { extends: ['@commitlint/config-conventional'], ignores: [message => /\[skip ci\]/m.test(message)] };
--- ref/ali-oss-6.23.0/test/node/fixtures/cleanAllBucket.js
+++
@@ -1,9 +0,0 @@
-const { cleanAllBucket } = require('../utils');
-const { oss: config } = require('../../config');
-const OSS = require('../../../lib/client');
-
-// eslint-disable-next-line no-console
-console.log(`cleanAllBucket...${new Date()}`);
-
-const store = new OSS(config);
-cleanAllBucket(store, 50, true);
